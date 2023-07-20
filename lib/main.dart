d067984edeede83771989c812bf157bb41808150
Thomas Rooyakkers <thomas.rooyakkers@aviobook.aero>
2023-07-20 12:38:34 +0200

    Refresh button + undo score

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
         create: (context) => ScoreState(),
         child: MaterialApp(
-          title: 'UWU',
+          title: 'KickerCounter',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
             useMaterial3: true,
           ),
           home: const MyHomePage(),
@@ -61,6 +61,14 @@ class ScoreState extends ChangeNotifier {
   void incrementScoreRight() {
     scoreRight++;
   }
+
+  void reduceScoreLeft() {
+    scoreLeft--;
+  }
+
+  void reduceScoreRight() {
+    scoreRight--;
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -72,38 +80,54 @@ class MyHomePage extends StatelessWidget {
     const style = TextStyle(
         color: Colors.white, fontWeight: FontWeight.normal, fontSize: 64);
     return Scaffold(
-        body: Row(children: [
-      Expanded(
-          child: GestureDetector(
-              onTap: () {
-                scoreState.incrementscoreLeft();
-                scoreState.calculateScore();
-              },
-              child: Container(
-                color: Colors.red,
-                child: Center(
-                  child: Text(
-                    scoreState.scoreLeft.toString(),
-                    style: style,
+      body: Row(children: [
+        Expanded(
+            child: GestureDetector(
+                onTap: () {
+                  scoreState.incrementscoreLeft();
+                  scoreState.calculateScore();
+                },
+                onLongPress: () {
+                  scoreState.reduceScoreLeft();
+                  scoreState.calculateScore();
+                },
+                child: Container(
+                  color: Colors.red,
+                  child: Center(
+                    child: Text(
+                      scoreState.scoreLeft.toString(),
+                      style: style,
+                    ),
                   ),
-                ),
-              ))),
-      Expanded(
-          child: GestureDetector(
-        onTap: () {
-          scoreState.incrementScoreRight();
-          scoreState.calculateScore();
-        },
-        child: Container(
-          color: Colors.blue,
-          child: Center(
-            child: Text(
-              scoreState.scoreRight.toString(),
-              style: style,
+                ))),
+        Expanded(
+            child: GestureDetector(
+          onTap: () {
+            scoreState.incrementScoreRight();
+            scoreState.calculateScore();
+          },
+          onLongPress: () {
+            scoreState.reduceScoreRight();
+            scoreState.calculateScore();
+          },
+          child: Container(
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                scoreState.scoreRight.toString(),
+                style: style,
+              ),
             ),
           ),
-        ),
-      )),
-    ]));
+        )),
+      ]),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            scoreState.resetScore();
+            scoreState.calculateScore();
+          },
+          child: const Icon(Icons.autorenew)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
